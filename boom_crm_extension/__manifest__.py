d4fd0e751b99efba219491e199765bf8d9b8fa5b
Jonathan <manuel200084@outlook.es>
2025-08-13 03:23:19 +0000

    [IMP] modulo

--- boom_crm_extension/__manifest__.py
+++ boom_crm_extension/__manifest__.py
@@ -3,15 +3,14 @@
     "summary": "Extensión del CRM para campos y flujo de aprobación",
     "version": "18.0.1.0.0",
     "category": "Sales/CRM",
-    "author": "Boom Solutions (tech test)",
+    "author": "Boom Solutions (Ericsson)",
     "license": "LGPL-3",
-    "website": "https://example.com",
     "depends": ["crm"],
     "data": [
         "views/crm_order_view.xml",
         "data/demo_data.xml"
     ],
     "demo": [],
-    "application": false,
-    "installable": true
+    "application": True,
+    "installable": True
 }
